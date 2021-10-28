0e42a48519af16246452d9e50060c5cd917f1454
Huy Nguyen <hyquaq@gmail.com>
2021-10-28 09:26:43 +0700

    quick commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,61 @@
 import "./app.scss";
+
+import Contacts from "./components/Contacts";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
-import Footer from "./components/Footer";
-import Contacts from "./components/Contacts";
 
 const cards = [
   {
-    name: "quiz",
-    srcImg: "/imgs/quiz.png",
-    url: "https://hyquaq.github.io/WebQuestion/",
+    name : "quiz",
+    srcImg : "/imgs/quiz.png",
+    url : "https://hyquaq.github.io/WebQuestion/",
   },
   {
-    name: "count down",
-    srcImg: "/imgs/countdown.png",
-    url: "https://hyquaq.github.io/25m/",
+    name : "count down",
+    srcImg : "/imgs/countdown.png",
+    url : "https://hyquaq.github.io/25m/",
   },
   {
-    name: "count day",
-    srcImg: "/imgs/countday.png",
-    url: "https://hyquaq.github.io/count-day/",
-  },
-  {
-    name: "Wallpaper Collection",
-    srcImg: "/imgs/desdev1.png",
-    url: "https://hyquaq.github.io/desdev-ex1/",
-  },
-  {
-    name: "Wallpaper Collection",
-    srcImg: "/imgs/desdev2.png",
-    url: "https://hyquaq.github.io/desdev-ex2/",
-  },
-  {
-    name: "Wallpaper Collection",
-    srcImg: "/imgs/desdev3.png",
-    url: "https://hyquaq.github.io/desdev-ex3/",
+    name : "count day",
+    srcImg : "/imgs/countday.png",
+    url : "https://hyquaq.github.io/count-day/",
   },
   {
     name : "Wallpaper Collection",
+    srcImg : "/imgs/desdev1.png",
+    url : "https://hyquaq.github.io/desdev-ex1/",
+  },
+  {
+    name : "Card film",
+    srcImg : "/imgs/desdev2.png",
+    url : "https://hyquaq.github.io/desdev-ex2/",
+  },
+  {
+    name : "toast",
+    srcImg : "/imgs/desdev3.png",
+    url : "https://hyquaq.github.io/desdev-ex3/",
+  },
+  {
+    name : "Card food",
     srcImg : "/imgs/desdev4.png",
     url : "https://hyquaq.github.io/desdev-ex4/",
   },
   {
-    name: "Wallpaper Collection",
-    srcImg: "/imgs/desdev5.png",
-    url: "https://hyquaq.github.io/desdev-ex5/",
+    name : "IMDB",
+    srcImg : "/imgs/desdev5.png",
+    url : "https://hyquaq.github.io/desdev-ex5/",
   },
   {
-    name: "Wallpaper Collection",
-    srcImg: "/imgs/desdev7.png",
-    url: "https://hyquaq.github.io/desdev-ex7/",
+    name : "pages",
+    srcImg : "/imgs/desdev7.png",
+    url : "https://hyquaq.github.io/desdev-ex7/",
   },
   {
-    name: "Wallpaper Collection",
-    srcImg: "/imgs/desdev9.png",
-    url: "https://hyquaq.github.io/desdev-ex9/",
+    name : "form login",
+    srcImg : "/imgs/desdev9.png",
+    url : "https://hyquaq.github.io/desdev-ex9/",
   },
 ];
 
@@ -65,14 +66,14 @@ const listSocials = [
     urlImgHover : "/imgs/icon/facebook-bold.png",
   },
   {
-    url: "https://www.youtube.com/channel/UCjjhWyzMfhhkXxc7qcHEdCA/",
-    urlImg: "/imgs/icon/youtube.png",
-    urlImgHover: "/imgs/icon/youtube-bold.png",
+    url : "https://www.youtube.com/channel/UCjjhWyzMfhhkXxc7qcHEdCA/",
+    urlImg : "/imgs/icon/youtube.png",
+    urlImgHover : "/imgs/icon/youtube-bold.png",
   },
   {
-    url: "https://github.com/hyquaq",
-    urlImg: "/imgs/icon/github.png",
-    urlImgHover: "/imgs/icon/github-bold.png",
+    url : "https://github.com/hyquaq",
+    urlImg : "/imgs/icon/github.png",
+    urlImgHover : "/imgs/icon/github-bold.png",
   },
 ];
 
@@ -84,7 +85,8 @@ function App() {
       <Header />
       <main>
         <Hero />
-        <Projects projects={cards} />
+        <Projects projects={
+    cards} />
         <Contacts listSocials={listSocials} />
       </main>
       <Footer />
